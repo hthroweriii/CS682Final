98786f25d43ff127c117cc2912dbf350f46825ba
hthroweriii <hthroweriii@nevada.unr.edu>
2015-12-13 17:40:17 -0800

    Ai Training Setup

--- snake.py
+++ snake.py
@@ -2,7 +2,7 @@ import pygame, random, sys, re
 import os
 from datetime import datetime
 from pygame.locals import *
-import learn_snake as ls
+import learn_snake import *
 
 
 class SnakeFrame:
@@ -27,6 +27,7 @@ class GameData:
 		self.dataSrc = ""
 		self.customFileName = ""
 		self.trainData = []
+		self.trainTargetOutput = []
 
 def collide(x1, x2, y1, y2, w1, w2, h1, h2):
 	if x1+w1>x2 and x1<x2+w2 and y1+h1>y2 and y1<y2+h2:
@@ -41,7 +42,7 @@ def wait():
 		if pygame.event.peek(KEYUP):
 			return 
 
-def interpretFromFile(filename, trainData):
+def interpretFromFile(filename, trainData, targetOutput):
 	frames = list()
 	outArray = []
 	data = open(filename, 'r')
@@ -60,8 +61,8 @@ def interpretFromFile(filename, trainData):
 		tempArray += xList
 		for x in range(0,50-len(yList)):
 			tempArray += [0]
-
 		trainData.append(tempArray)
+		targetOutput.append(direction)
 	data.close()
 	#for x in frames:
 	#	tempArray = tempArray + [x]
@@ -122,7 +123,7 @@ def fileExits(name):
 
 def replayRoutine( gd ):
 	print "Replay Beginning"
-	gd.loadedData = interpretFromFile(gd.dataSrc, gd.trainData)
+	gd.loadedData = interpretFromFile(gd.dataSrc, gd.trainData, gd.trainTargetOutput)
 	s=gd.screen
 	appleimage = gd.appleimage
 	appleimage.fill((0, 255, 0))
@@ -169,7 +170,7 @@ def regularGameRoutine( gd):
 	
 	#Training Setup
 	if gd.loadData:
-		gd.loadedData = interpretFromFile(gd.dataSrc, gd.trainData)
+		gd.loadedData = interpretFromFile(gd.dataSrc, gd.trainData, gd.trainTargetOutput)
 		print gd.trainData
 	frameData = list()
 	lastValidFrame = 0
@@ -272,6 +273,116 @@ def regularGameRoutine( gd):
 		pygame.display.update()
 		totalFrames += 1
 
+
+def aiGameRoutine( gd , ps):
+	def die(screen, score):
+		#Declare Variables
+		f=pygame.font.SysFont('Arial', 30)
+		t=f.render('Your score was: '+str(score), True, (0, 0, 0))
+		date = datetime.now()
+
+		#Die Sequence
+		screen.blit(t, (10, 270))
+		pygame.display.update()
+
+		#write weights to file
+
+		pygame.time.wait(2000)
+
+		sys.exit(0)
+	
+	#Training Setup
+	
+	
+	applepos = list()
+	startSquaresX = [290, 290, 290, 290, 290, 290, 290, 290, 290, 290, 290, 290, 290, 310, 310, 310, 310, 310, 310, 310, 310, 310, 310, 310, 310, 310,\
+	330, 330, 330, 330, 330, 330, 330, 330, 330, 330, 330, 330, 330, 350, 350, 350, 350, 350, 350, 350, 350, 350, 350, 350, 350, 350,\
+	370, 370, 370, 370, 370, 370, 370, 370, 370, 370, 370, 370, 370, 390, 390, 390, 390, 390, 390, 390, 390, 390, 390, 390, 390, 390,\
+	410, 410, 410, 410, 410, 410, 410, 410, 410, 410, 410, 410, 410, 410, 430, 430, 430, 430, 430, 430, 430, 430, 430, 430, 430, 430]
+	startSquaresY = [290, 270, 250, 230, 210, 190, 170, 150, 130, 110,  90,  70,  50,  50,  70,  90, 110, 130, 150, 170, 190, 210, 230, 250, 270, 290,\
+	290, 270, 250, 230, 210, 190, 170, 150, 130, 110,  90,  70,  50,  50,  70,  90, 110, 130, 150, 170, 190, 210, 230, 250, 270, 290,\
+	290, 270, 250, 230, 210, 190, 170, 150, 130, 110,  90,  70,  50,  50,  70,  90, 110, 130, 150, 170, 190, 210, 230, 250, 270, 290,\
+	290, 270, 250, 230, 210, 190, 170, 150, 130, 110,  90,  70,  50,  50,  70,  90, 110, 130, 150, 170, 190, 210, 230, 250, 270, 290]
+
+
+
+	#General Game Setup
+	gd.startSquares = 5
+	#Standard Game Start
+	xs = list(startSquaresX[0:gd.startSquares])
+	ys = list(startSquaresY[0:gd.startSquares])
+	dirs = 0 
+	score = 0
+	applepos = (random.randint(0, 590), random.randint(0, 590))
+
+	s= gd.screen
+	appleimage = gd.appleimage
+	appleimage.fill((0, 255, 0))
+	img = gd.img
+	img.fill((255, 0, 0))
+	f = gd.font
+	clock = pygame.time.Clock()
+
+	#Initial Draw
+	s.fill((255, 255, 255))	
+	for i in range(0, len(xs)):
+		s.blit(img, (xs[i], ys[i]))
+	s.blit(appleimage, applepos)
+	t=f.render(str(score), True, (0, 0, 0))
+	s.blit(t, (10, 10))
+	pygame.display.update()
+
+	while True:
+		if gd.humanTrain and gd.trainPause:
+			wait()
+		events = pygame.event.get()
+		frameData.append(SnakeFrame(xs,ys, applepos, dirs))
+		state = []
+		state += applepos + [dirs] + xs
+		for x in range(0,50-len(xs)):
+			state += [0]
+		state += xs
+		for x in range(0,50-len(ys)):
+			state += [0]
+		dirs = ps.predict(state)
+
+		i = len(xs)-1
+		while i >= 2:
+			if collide(xs[0], xs[i], ys[0], ys[i], 20, 20, 20, 20):
+				die(s, score)
+			i-= 1
+		#Apple Collision
+		if collide(xs[0], applepos[0], ys[0], applepos[1], 20, 10, 20, 10):
+			score+=1
+			xs.append(700)
+			ys.append(700)
+			applepos=(random.randint(0,590),random.randint(0,590))
+			lastValidFrame = totalFrames
+		#Walls
+		if xs[0] < 0 or xs[0] > 580 or ys[0] < 0 or ys[0] > 580: 
+			die(s, score)
+		i = len(xs)-1
+		while i >= 1:
+			xs[i] = xs[i-1]
+			ys[i] = ys[i-1]
+			i -= 1
+		if dirs==0:ys[0] += 20
+		elif dirs==1:xs[0] += 20
+		elif dirs==2:ys[0] -= 20
+		elif dirs==3:xs[0] -= 20	
+		s.fill((255, 255, 255))	
+		for i in range(0, len(xs)):
+			s.blit(img, (xs[i], ys[i]))
+		s.blit(appleimage, applepos)
+		t=f.render(str(score), True, (0, 0, 0))
+		s.blit(t, (10, 10))
+		pygame.display.update()
+		totalFrames += 1
+
+
+
+
+
 def main():
 	#Config
 		gameData = GameData()
@@ -285,24 +396,16 @@ def main():
 		gameData.img.fill((255, 0, 0))
 		gameData.font = pygame.font.SysFont('Arial', 20)
 		if gameData.playData and gameData.loadData:
-			if gameData.humanTrain:
-				playSnake = MLP(2, 1)
-				playSnake.add(Linear(103,1000))
-				playSnake.add(Sigmoid(1000))
-				playSnake.add(Linear(1000,4))
-				playSnake.add(Softmax(4))
-				criterion = NLLLoss(103)
-
-
-
-
-
-
-
-
-				
-			else:
 				replayRoutine( gameData )
+		if gameData.humanTrain:
+			playSnake = MLP(2, 1)
+			playSnake.add(Linear(103,1000))
+			playSnake.add(Sigmoid(1000))
+			playSnake.add(Linear(1000,4))
+			playSnake.add(Softmax(4))
+			criterion = NLLLoss(103)
+			playSnake.train(gameData.trainData[:-1], gameData.trainTargetOutput[1:] ,  playSnake, criterion)
+			aiGameRoutine( gameData, playSnake)
 		else:
 			regularGameRoutine( gameData )
 
